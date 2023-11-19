d2e3291312b3e665d079b124c0f4ba163148029f
Ehsan <esihazar@gmail.com>
2023-11-19 11:19:17 -0500

    Three Pages added to my application

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+// Configuring Router for my application
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
 
-createApp(App).mount('#app')
+// **** Setting Up Vue Router inside main.js file **** //
+
+const router = VueRouter.createWebHistory(process.env.BASE_URL); // process.env.BASE_URL Its a variable containing the base url of your application
+
+// Adding Router to your Vue app using .use method
+const app = createApp(App);
+app.use(router); // Adding history mode configuration to your app
+
+// To display matching page for given route use router-view component
+
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>;
+
+app.mount("#app");
